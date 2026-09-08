b3dc8eaead6a16d77c95f8a70cadf840c2662f79
abidsubair7 <abidsubair7@gmail.com>
2026-09-08 22:36:00 +0530

    Add advertisement migration

---
+++ core/migrations/0002_alter_advertisement_options.py
@@ -0,0 +1,5 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [("core", "0001_initial")]
+    operations = [migrations.AlterModelOptions(name="advertisement", options={"ordering": ["-created_at"]})]
